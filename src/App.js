835bbc49c5d215890e5cbad391fe79bcbc0f959a
djdeo <deoseoer@gmail.com>
2021-02-19 11:25:06 +0800

    feat: chakra style props prac

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, Button, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
+        <Grid minH="100vh" p={3} grap={2} gridColumn={3} gridRow={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+          <Box
+            m={2}
+            py={6}
+            rounded="md"
+            w="120px"
+            h="80px"
+            color="black.500"
+            bg="tomato"
+            opacity="0.85"
+            _hover={{ opacity: 1, border: '1px solid red' }}
+            boxShadow="xl"
+            justifySelf="center"
+          >
+            Center Box
+          </Box>
         </Grid>
+        <Box role="group">
+          <Box
+            _hover={{ fontWeight: 'semibold' }}
+            _groupHover={{ color: 'teal' }}
+          >
+            hover me
+          </Box>
+        </Box>
+        <Box
+          w="120px"
+          h="80px"
+          bgGradient="linear(to-t, blue.200, green.200)"
+        ></Box>
+        <Button as="a" target="_blank" variant="outline">
+          I am a link{' '}
+        </Button>
       </Box>
     </ChakraProvider>
   );
